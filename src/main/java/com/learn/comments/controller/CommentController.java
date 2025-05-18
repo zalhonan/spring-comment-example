995107a71685ec233ba6cc4e9490f788e4e0c235
Konstantin Kuzminov <konstantin.kuzminov@pashapay.az>
2025-05-18 15:27:33 +0200

    controller main methods

--- src/main/java/com/learn/comments/controller/CommentController.java
+++ src/main/java/com/learn/comments/controller/CommentController.java
@@ -2,9 +2,7 @@ package com.learn.comments.controller;
 
 import com.learn.comments.model.Comment;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,24 @@ public class CommentController {
         );
     }
 
+    @GetMapping("/{uuid}")
+    public Comment getCommentByUuid(@PathVariable String uuid) {
+        return Comment.builder().uuid(uuid).content("This is a comment").build();
+    }
 
+    @PostMapping
+    public String postComment() {
+        return java.util.UUID.randomUUID().toString();
+    }
+
+    @PutMapping("/{uuid}")
+    public String updateComment(@RequestParam String uuid) {
+        return String.format("Comment with UUID %s updated successfully.", uuid);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public String deleteComment(@RequestParam String uuid) {
+        return String.format("Comment with UUID %s deleted successfully.", uuid);
+    }
 }
+
